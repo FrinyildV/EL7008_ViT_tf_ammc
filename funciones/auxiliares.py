274f96dfbd6fe41676e902f58ced855a6b7b41ae
Angelo Muñoz <angelomunozc@gmail.com>
2023-12-23 04:38:05 -0400

    Update auxiliares.py

--- funciones/auxiliares.py
+++ funciones/auxiliares.py
@@ -9,6 +9,56 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm, trange
 import cv2 
 
+
+
+#%%%%
+
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from google.colab.patches import cv2_imshow
+
+from tqdm import tqdm, trange
+
+import cv2
+
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from torch.nn import CrossEntropyLoss
+from torch.utils.data import DataLoader
+
+from torchvision.transforms import ToTensor
+from torchvision.datasets.mnist import MNIST
+
+np.random.seed(0)
+torch.manual_seed(0)
+
+
+
+from torch import Tensor
+from einops import rearrange , reduce, repeat
+from einops.layers.torch import Rearrange, Reduce
+
+from sklearn.model_selection import train_test_split
+
+from sklearn import metrics as metsk
+import matplotlib.pyplot as plt
+
+from torchsummary import summary
+#%%%
+
+
+
+
+
 class Lector(Dataset):
     def __init__(self, data, modo,ruta_images, transform=None):
         self.data = data #df
