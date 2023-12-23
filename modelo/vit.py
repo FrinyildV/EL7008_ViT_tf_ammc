badb69b6763cbfd8cd34fb2b2b0710a9e7a68ea7
Angelo Muñoz <angelomunozc@gmail.com>
2023-12-23 18:12:37 -0400

    Update vit.py

--- modelo/vit.py
+++ modelo/vit.py
@@ -31,6 +31,50 @@ from einops.layers.torch import Rearrange, Reduce
 
 
 
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
+from PIL import Image
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from google.colab.patches import cv2_imshow
+
+from tqdm import tqdm, trange
+
+import cv2
+
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from torch.nn import CrossEntropyLoss
+from torch.utils.data import DataLoader
+
+from torchvision.transforms import ToTensor
+from torchvision.datasets.mnist import MNIST
+
+np.random.seed(0)
+torch.manual_seed(0)
+
+
+
+from torch import Tensor
+from einops import rearrange , reduce, repeat
+from einops.layers.torch import Rearrange, Reduce
+
+from sklearn.model_selection import train_test_split
+
+from sklearn import metrics as metsk
+import matplotlib.pyplot as plt
+
+from torchsummary import summary
+
+
+
+
 #%%%%%%%%%%%%%%%
 
 #%%%%%%% Bloques de la red:
